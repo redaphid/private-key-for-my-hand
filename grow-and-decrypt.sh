b47563bf1aca3af389d2bf53214ca9b8d0dcd9bc
redaphid <iam@aaronherres.com>
2019-04-11 20:35:02 -0400

    looks like we can encrypt, compress, and decrypt. It's not identical, but is that important for a crypto key :)

--- grow-and-decrypt.sh
+++ grow-and-decrypt.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-key_header="-----BEGIN OPENSSH PRIVATE KEY-----"
-key_footer="-----END OPENSSH PRIVATE KEY-----"
+key_header="-----BEGIN OPENSSH PRIVATE KEY-----\n"
+key_footer="\n-----END OPENSSH PRIVATE KEY-----"
 
-key=`cat $1`
-openssl enc -d -aes-256-cbc -salt $key
+key=`openssl enc -d -aes-256-cbc -salt -in $1 | gzip -d | base64`
+key="$key_header$key$key_footer"
+echo $key
 # echo $key | base64 -D | gzip | openssl enc -aes-256-cbc -salt

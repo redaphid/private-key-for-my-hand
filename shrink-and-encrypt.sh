25aa927d75ad48f11934620d776c2c824d0c0515
redaphid <iam@aaronherres.com>
2019-04-11 20:12:38 -0400

    compresses and encrypts a public key without DER support. Probably. Haven't tried undoing it yet

--- shrink-and-encrypt.sh
+++ shrink-and-encrypt.sh
@@ -5,5 +5,5 @@ key_footer="-----END OPENSSH PRIVATE KEY-----"
 key=`cat $1`
 key=${key#$key_header}
 key=${key%$key_footer}
-echo $key
-# echo $key | base64 -D | gzip | openssl enc -aes-256-cbc -salt
+
+echo $key | base64 -D | gzip | openssl enc -aes-256-cbc -salt
